1c14ada1d30fa83346df2e18d99b7218ea496b41
Kranthi Sirimalla <kranthi.siri95@gmail.com>
2022-08-05 16:40:43 -0400

    player code

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
 "use strict";
 
 $(document).ready(() => {
-  
+
     //set name and no of cards if exists in sessionStorage and 
     // create a new player object and call new game method to start the game
-    $("#player_name3").text(sessionStorage.getItem("player_name"));
-    $("#num_cards").text(sessionStorage.getItem("num_cards"));
-
+    const name = sessionStorage.getItem("player_name");
+    const num_cards = sessionStorage.getItem("num_cards");
+    if (name && num_cards) {
+        $("#player_name3").text(name);
+        $("#player_name").val(name);
+        $("#num_cards").val(num_cards);
+        const player = new Player(name,num_cards);
+        player.newGame();
+    }
 
     // handle save_settings click method and reload the page
     $("#save_settings").click(() => {
         const player_name = $("#player_name").val();
+        const cards = $("#num_cards").val();
         let isValid = true;
         if (player_name === "") {
             isValid = false;
             $("#player_name").next().text("Please enter a valid player name.");
         }
 
-        if (num_cards === "") {
+        if (cards === "") {
             isValid = false;
-            $("#num_cards").next().text("Please enter a valid player name.");
+            $("#num_cards").next().text("Please select a cards.");
         }
 
         if (isValid) {
             sessionStorage.setItem("player_name", player_name);
-            sessionStorage.setItem("num_cards", num_cards);
+            sessionStorage.setItem("num_cards", cards);
             window.location.reload();
         }
         $("#player_name").focus();
     });
+
     $("#player_name").focus();
 }); 
--- player.js
+++ player.js
@@ -10,7 +10,7 @@ class Player {
         this.clickCounter = 0;  // to know about which click we are in
         this.imageOrder = [];
         this.firstImageId = null;
-        this.secondImageId = null;
+        this.firstImagePath = null;
 
     }
 
@@ -25,11 +25,51 @@ class Player {
 
         // if no of cards are 24;then we will take 12 images from ImageArrayList and double it to make 24 
         // show these 24 images by the random order from imageOrder variable
+        this.imageOrder = [];
+        this.randomOrder();
+        console.log(this.imageOrder);
+        let htmlString = "";
+        let rowString = "<div class='row'>";
+        for (let x = 0; x < this.imageOrder.length; x++) {
+            const val = this.imageOrder[x] - 1;
+            let path = "";
+            if (val > 23) {
+                path = ImageArrayList[val - 24];
+            } else {
+                path = ImageArrayList[val]
+            }
+            console.log(path);
+            const element = `<a id="id_${this.imageOrder[x]}" href="#" class="card-element"> 
+                <img src="images/back.png" class="card-img show" />
+                </a>`;
+            // <img src="${path}" class="card-img hidden" />
+
+            rowString += element;
+            if ((x > 0 && (x + 1) % 8 == 0) || x == this.imageOrder.length - 1) {
+                rowString += "</div>"
+                htmlString += String(rowString).substring(0);
+                rowString = "<div class='row'>";
+            }
+        }
+
+        document.querySelector('#cards').innerHTML = htmlString;
+        const elements = document.getElementsByClassName('card-element');
+
+        for (let i = 0; i < elements.length; i++) {
+            const id = elements[i].id.substring(3);
+            elements[i].addEventListener("click", () => this.handleClick(id));
+        }
     }
 
     randomOrder() {
         // method to return random order of index's between 1 to noOfCards
         // this order will be saved in imageOrder
+        while (this.imageOrder.length < this.noOfCards) {
+            const val = Math.floor(Math.random() * this.noOfCards) + 1;
+            if (this.imageOrder.indexOf(val) === -1) {
+                this.imageOrder.push(val);
+            }
+        }
     }
     handleClick(event) {
         // method to handle click event on images
@@ -47,9 +87,53 @@ class Player {
         // finally check if all images are opened
         // if yes , show the highest score and call saveHighestScore method
         // if no, continue
+        if (event == this.firstImageId) {
+            return;
+        }
+        // const val = this.imageOrder[event] - 1;
+
+        // let path = `images/card_${event}.png`;
+        const val = this.imageOrder[event] - 1;
+        console.log(val)
+            let path = "";
+            if (val > 23) {
+                path = ImageArrayList[val - 24];
+            } else {
+                path = ImageArrayList[val]
+            }
+            console.log(path);
+        const element = document.getElementById('id_' + event);
+        const image = element.getElementsByTagName('img');
+        image[0].src = path;
+        this.totalScore++;
+        if (this.clickCounter == 0) {
+            this.clickCounter++;
+            this.firstImageId = event;
+            this.firstImagePath = path;
+        } else {
+            if (this.firstImagePath == path) {
+                [this.firstImageId, event].forEach(row => {
+                    const element = document.getElementById('id_' + row);
+                    const image = element.getElementsByTagName('img');
+                    image[0].style.display = "none";
+                })
+            } else {
+                setTimeout(() => {
+                    [this.firstImageId, event].forEach(row => {
+                        const element = document.getElementById('id_' + row);
+                        const image = element.getElementsByTagName('img');
+                        image[0].src = "images/back.png";
+                    })
+                    this.firstImageId = null;
+                    this.firstImagePath = null;
+                    this.clickCounter = 0;
+                }, 2000);
+            }
+        }
+        $("#high_score").text(this.totalScore);
     }
 
-    saveHighestScore(){
+    saveHighestScore() {
         // method to save highscore of player name with no of cards into localstorage
     }
 
